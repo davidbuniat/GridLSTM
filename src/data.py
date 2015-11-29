e45aaa5f4f38a5afadd205e0ab35523e9f063199
David <david.buniatyan@gmail.com>
2015-11-29 17:58:22 +0000

    ML Network is working with 2000 dataset

---
+++ src/data.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Loading Data
+from __future__ import print_function
+
+import scipy
+import numpy as np
+import theano
+import theano.tensor as T
+import png
+import os
+import random
+import itertools
+
+
+labels = {}
+labels_rev = {}
+index_temp = 0
+mean = 0
+std = 0  
+
+def load_dataset(number_training, row_count, column_count, plane_count, DEBUG):
+    print ("Loading data...")
+    # We first define a download function, supporting both Python 2 and 3.
+
+    # We then define functions for loading afreightData images and labels.
+    # For convenience, they also download the requested files if needed.
+
+    def load_images(pathname):
+
+        cur_dir = os.path.dirname(os.path.realpath(__file__)) #Current Directory
+        
+        listing = os.listdir(cur_dir+pathname)
+        p = [(255,0,0, 0,255,0, 0,0,255),
+            (128,0,0, 0,128,0, 0,0,128)]  
+
+        data = np.empty((number_training,row_count*column_count, plane_count))
+        index = 0
+        for file in listing:
+            r = png.Reader(cur_dir+pathname+'/'+file)      # binary mode is important
+            pngData = r.read()[2]
+
+            image_2d = np.vstack(itertools.imap(np.uint16, pngData))
+            image_3d = np.reshape(image_2d, (row_count,column_count, plane_count))
+
+            image_1d = np.reshape(image_2d, (row_count*column_count, plane_count))
+            data[index] = image_1d
+            index = index + 1
+            #For debuggin
+            if DEBUG and index>9:
+                break
+        
+        return data[:index]
+
+    # We read the training and test set images and labels.
+    X = load_images('/../datasets/afreightdata/data')
+    Y = load_images('/../datasets/afreightdata/label')
+   
+    def labelling(y):
+        new_y = np.empty((number_training, row_count*column_count, 1))
+        index = 0
+        for i in xrange(1,len(y)):
+            for j in xrange(1,len(y[i])):
+                id = str(y[i][j][0])+'-'+str(y[i][j][1])+"-"+str(y[i][j][2])
+
+                if id in labels:
+                    new_y[i][j][0] = labels[id]
+                else:
+                    labels[id] = index
+                    labels_rev[index] = y[i][j]
+                    index = index + 1
+                    new_y[i][j][0] = labels[id]
+                pass
+            pass
+
+        print("labels added " +str(index))
+        new_y = new_y/np.float32(str(index))
+        index_temp = index
+          
+        return new_y
+
+    # Normalizing Data
+    X = X / np.float32(256)
+    Y = labelling(Y)
+
+    train_count = 250
+    val_count = 150
+    test_count = 55
+
+    if DEBUG:
+        train_count = 5
+        val_count = 3
+        test_count = 2
+
+    rand_array = random.sample(xrange(0,len(X)), len(X));
+    val_array = rand_array[:val_count]
+    test_array = rand_array[:test_count]
+    train_array = rand_array[:train_count]
+
+    print("Loaded "+str(len(X)) + " images")
+    return X[train_array], Y[train_array], X[val_array], Y[val_array], X[test_array], Y[test_array]
+
+def saveImage(Y, name, row_count, column_count, plane_count):
+    new_y = np.empty((row_count*column_count, plane_count))
+    for i in range(len(new_y)):
+        new_y[i] = labels_rev[int(round(Y[i][0]*len(labels_rev))) ]
+    image = np.reshape(new_y, (row_count, column_count, plane_count))
+    scipy.misc.imsave(name, image)
+
+
---
+++ src/main.py
@@ -0,0 +1,139 @@
+
+from __future__ import print_function
+
+import scipy
+import numpy as np
+import theano
+import theano.tensor as T
+import lasagne
+
+import png
+import os
+import random
+import itertools
+
+import data as dt
+
+#Parameters
+DEBUG = True
+number_training = 455
+row_count = 120
+column_count = 160
+plane_count = 3
+
+
+# Min/max sequence length
+MIN_LENGTH = 50
+MAX_LENGTH = 19200
+# Number of units in the hidden (recurrent) layer
+N_HIDDEN = 4
+# Number of training sequences in each batch
+N_BATCH = 10
+# Optimization learning rate
+LEARNING_RATE = .001
+# All gradients above this will be clipped
+GRAD_CLIP = 100
+# How often should we check the output?
+EPOCH_SIZE = 10
+# Number of epochs to train the net
+NUM_EPOCHS = 25
+
+if DEBUG:
+    NUM_EPOCHS = 1
+    EPOCH_SIZE = 5
+
+BATCH_SIZE = 455
+length = 19200
+
+def main(num_epochs=NUM_EPOCHS):
+    print("Building network ...")
+
+
+    l_in = lasagne.layers.InputLayer((None, MAX_LENGTH, 3))
+    batchsize, seqlen, _ = l_in.input_var.shape
+
+    l_forward = lasagne.layers.LSTMLayer(
+        l_in, N_HIDDEN, grad_clipping=GRAD_CLIP,
+        nonlinearity=lasagne.nonlinearities.tanh)
+
+    l_backward = lasagne.layers.LSTMLayer(
+        l_in, N_HIDDEN, grad_clipping=GRAD_CLIP,
+        nonlinearity=lasagne.nonlinearities.tanh, backwards=True)
+
+    l_recurrent = lasagne.layers.ElemwiseSumLayer([l_forward, l_backward])
+
+    l_reshape = lasagne.layers.ReshapeLayer(l_recurrent,
+                                       (batchsize*seqlen, N_HIDDEN ))
+
+    softmax = lasagne.nonlinearities.softmax
+
+    l_rec_out = lasagne.layers.DenseLayer(l_reshape, num_units=1)
+
+    l_softmax = lasagne.layers.DenseLayer(l_rec_out, num_units=157, nonlinearity=softmax)
+
+    l_out = lasagne.layers.ReshapeLayer(l_softmax, (batchsize, seqlen, 1)) 
+
+    # Now, we'll concatenate the outputs to combine them.
+    #l_sum = lasagne.layers.ConcatLayer([l_forward, l_backward], 2)
+
+    #l_shp = lasagne.layers.ReshapeLayer(l_sum, (-1, N_HIDDEN))
+
+    # Our output layer is a simple dense connection, with 1 output unit
+    #l_final = lasagne.layers.DenseLayer(l_shp, num_units=1, nonlinearity=lasagne.nonlinearities.tanh)
+    
+    #l_out = lasagne.layers.ReshapeLayer(l_final, (batchsize, seqlen, 1))
+
+    target_values = T.tensor3('target_output')
+
+    # lasagne.layers.get_output produces a variable for the output of the net
+    network_output = lasagne.layers.get_output(l_out)
+
+    # The value we care about is the final value produced for each sequence
+    predicted_values = network_output
+
+    # Our cost will be mean-squared error
+    #cost = T.mean((predicted_values - target_values)**2)
+    cost = lasagne.objectives.categorical_crossentropy(predicted_values, target_values)
+    cost = cost.mean()
+
+    # Retrieve all parameters from the network
+    all_params = lasagne.layers.get_all_params(l_out)
+
+    # Compute SGD updates for training
+    print("Computing updates ...")
+    updates = lasagne.updates.nesterov_momentum(cost, all_params, LEARNING_RATE)
+    
+    # Theano functions for training and computing cost
+    print("Compiling functions ...")
+    train = theano.function([l_in.input_var, target_values],
+                            cost, updates=updates)
+    compute_cost = theano.function(
+        [l_in.input_var, target_values], cost)
+
+    get_prediction = theano.function([l_in.input_var], predicted_values)
+
+    # We'll use this "validation set" to periodically check progress
+    X_train, y_train, X_val, y_val, X_test, y_test = dt.load_dataset(BATCH_SIZE, row_count, column_count, plane_count, DEBUG)
+ 
+    print("Training ...")
+    #print(get_prediction(X_train[0:1]))
+
+    try:
+        index = 0
+        for epoch in range(num_epochs):
+            X = X_train[EPOCH_SIZE*epoch:EPOCH_SIZE*(epoch+1)]
+            y = y_train[EPOCH_SIZE*epoch:EPOCH_SIZE*(epoch+1)]
+            train(X, y)
+
+            cost_val = compute_cost(X_val, y_val)
+            cost_test = compute_cost(X_test, y_test)
+            print("Epoch {} validation cost = {}  test cost = {} ".format(epoch, cost_val, cost_test))
+
+        dt.saveImage(y_test[0], "results/y_GT.png",row_count, column_count,  plane_count)
+        dt.saveImage(get_prediction(X_test)[0], "results/y_output.png", row_count, column_count,  plane_count)
+
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    main()
